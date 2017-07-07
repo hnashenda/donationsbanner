2bf1cb16f35395675184610230572bc54492be9d
PC <hubert.nashenda@gmail.com>
2017-07-07 05:12:03 +0200

    test

--- banner/app_limit.py
+++ banner/app_limit.py
@@ -21,21 +21,7 @@ class ShopifyConnection(Connection, object):
 		# Get this users last UID
 		last_call_time = memcache.get(uid+"_last_call_time")
 		last_call_value = memcache.get(uid+"_last_call_value")
-		if last_call_time and last_call_value:
-			# Calculate how many tokens are regenerated
-			now = datetime.datetime.utcnow()
-			delta = rate * ((now - last_call_time).seconds)
-
-			# If there is no change in time then check what last call value was
-			if delta == 0:
-				tokensAvailable = min(capacity, capacity - last_call_value)
-			# If there was a change in time, how much regen has occurred
-			else:            
-				tokensAvailable = min(capacity, (capacity - last_call_value) + delta)
-
-			# No tokens available can't call
-			if tokensAvailable <= min_interval:
-				raise pyactiveresource.connection.ConnectionError(message="No tokens available for: " + str(uid))
+		
 
 	def _open(self, *args, **kwargs):
 		uid = self.site.split("https://")[-1].split(".myshopify.com")[0]
@@ -43,8 +29,7 @@ class ShopifyConnection(Connection, object):
 		retries = 0
 		while True:
 			try:
-				#self.consume_token(uid, 40, 1.95, 0.05)
-				self.consume_token(234, 40, 1.95, 0.05)
+				self.consume_token(uid, 40, 1.95, 0.05)
 				self.response = super(ShopifyConnection, self)._open(*args, **kwargs)
 				return self.response
 			except (ConnectionError, ServerError) as err:
