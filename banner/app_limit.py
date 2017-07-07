ac453088b1ae13d2bf112a70a307b06804bfc699
PC <hubert.nashenda@gmail.com>
2017-07-07 19:21:48 +0200

    token parameters

--- banner/app_limit.py
+++ banner/app_limit.py
@@ -16,12 +16,27 @@ class ShopifyConnection(Connection, object):
 				format=formats.JSONFormat):
 		super(ShopifyConnection, self).__init__(site, user, password, timeout, format)
 
-	def consume_token(uid, capacity, rate, min_interval=0):
+	def consume_token(uid, capacity, rate, min_interval):
 	# Your rate limiting logic here
 		# Get this users last UID
 		last_call_time = memcache.get(uid+"_last_call_time")
 		last_call_value = memcache.get(uid+"_last_call_value")
 		
+		if last_call_time and last_call_value:
+            # Calculate how many tokens are regenerated
+            now = datetime.datetime.utcnow()
+            delta = rate * ((now - last_call_time).seconds)
+
+            # If there is no change in time then check what last call value was
+            if delta == 0:
+                tokensAvailable = min(capacity, capacity - last_call_value)
+            # If there was a change in time, how much regen has occurred
+            else:            
+                tokensAvailable = min(capacity, (capacity - last_call_value) + delta)
+
+            # No tokens available can't call
+            if tokensAvailable <= min_interval:
+                raise pyactiveresource.connection.ConnectionError(message="No tokens available for: " + str(uid))
 
 	def _open(self, *args, **kwargs):
 		uid = self.site.split("https://")[-1].split(".myshopify.com")[0]
