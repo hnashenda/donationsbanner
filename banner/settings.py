028ed78ca5ec435e70e5f4ea33f7537990c92371
PC <hubert.nashenda@gmail.com>
2017-07-14 13:56:55 +0200

    os

--- banner/settings.py
+++ banner/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 #import os
 import os
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'banner.settings'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'banner.settings'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
