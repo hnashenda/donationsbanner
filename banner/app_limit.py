0a9af0bd01f2bdf8e85160175a91cc52caaa852d
PC <hubert.nashenda@gmail.com>
2017-07-15 14:55:42 +0200

    add -a

--- banner/app_limit.py
+++ banner/app_limit.py
@@ -1,5 +1,6 @@
 import logging
 #from google.appengine.api import memcache
+from math import ceil
 import datetime
 from datetime import date, timedelta
 from django.conf import settings
@@ -31,10 +32,11 @@ import shopify
 #client.set('key1', 'value1')
 #cache.set('key1', 'value1')
 #r = client.get('key1')
+#start_time = datetime.datetime.utcnow()
 #print ("Stored to memcached, will auto-expire after 15 seconds")
 #print (cache.get("key1"))
-
-
+#print ("time is",start_time)
+#CYCLE = 0.5
 # Store the response from the last request in the connection object
 #class ShopifyConnection(pyactiveresource.connection.Connection):
 class ShopifyConnection(Connection, object):
@@ -46,39 +48,58 @@ class ShopifyConnection(Connection, object):
 
 	def consume_token(self, uid, capacity, rate, min_interval):
 		# Get this users last UID
+		start_time = datetime.datetime.utcnow()
+		print ("time is",start_time)
+		CYCLE = 0.5
 		last_call_time = cache.get(uid+"_last_call_time")
 		last_call_value = cache.get(uid+"_last_call_value")
+		stop_time = datetime.datetime.utcnow()
 		
-		print("the last call time",last_call_time)
-		print("the last call value",last_call_value)
+		print ("time stop is",stop_time)
+		processing_duration = (stop_time - start_time).total_seconds()
+		
+		#wait_time = timedelta(seconds=CYCLE - processing_duration.total_seconds())
+		wait_time = ceil(CYCLE - processing_duration)
+		
+		print('the duration is',processing_duration)
+		print('the wait is',wait_time)
+		#print("the last call time",last_call_time)
+		#print("the last call value",last_call_value)
 		#start_time = Time.now
 		
-		if last_call_time and last_call_value:
+		sleep(wait_time) 
+		print('sleeping')
+		if wait_time > 0:
+			start_time = datetime.datetime.utcnow()	
+			print('okay',start_time)
+		
+		#if last_call_time and last_call_value:
 			# Calculate how many tokens are regenerated
-			now = datetime.datetime.utcnow()
-			delta = rate * ((now - last_call_time).seconds)
+		#	now = datetime.datetime.utcnow()
+		#	delta = rate * ((now - last_call_time).seconds)
 
 			# If there is no change in time then check what last call value was
-			if delta == 0:
-				tokensAvailable = min(capacity, capacity - last_call_value)
+			#if delta == 0:
+			#	tokensAvailable = min(capacity, capacity - last_call_value)
 			# If there was a change in time, how much regen has occurred
-			else:            
-				tokensAvailable = min(capacity, (capacity - last_call_value) + delta)
+		#	else:            
+		#		tokensAvailable = min(capacity, (capacity - last_call_value) + delta)
 
 			# No tokens available can't call
-			if tokensAvailable <= min_interval:
-				raise pyactiveresource.connection.ConnectionError(message="No tokens available for: " + str(uid))
+		#	if tokensAvailable <= min_interval:
+		#		raise pyactiveresource.connection.ConnectionError(message="No tokens available for: " + str(uid))
 
 
 	def _open(self, *args, **kwargs):
 		uid = self.site.split("https://")[-1].split(".myshopify.com")[0]
 		self.response = None
 		retries = 0
-		#print("the open motiv")
+		print("baby it is real")
 		while True:
 			try:
 				#self.consume_token(uid, 40, 2, 0.05)
 				self.consume_token(uid, 40, 1.95, 0.05)
+				print("the open motiv", uid)
 				self.response = super(ShopifyConnection, self)._open(*args, **kwargs)
 				return self.response
 				#
@@ -92,8 +113,10 @@ class ShopifyConnection(Connection, object):
 				retries += 1
 				if retries > settings.SHOPIFY_MAX_RETRIES:
 					self.response = err.response
-					logging.error("Logging error for _open ShopifyConnection: " + str(uid) + ":" + str(err.message))
+					#logging.error("Logging error for _open ShopifyConnection: " + str(uid) + ":" + str(err.message))
+					print("the error")
 					raise
-				sleep(settings.SHOPIFY_RETRY_WAIT)
+				#sleep(settings.SHOPIFY_RETRY_WAIT)
+				sleep(20)
 				
 shopify.base.ShopifyConnection = ShopifyConnection				
